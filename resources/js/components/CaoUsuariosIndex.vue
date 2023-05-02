a466a3284e2c2cc72a634c1fa742cbbbc7eddf94
Eduardo Dantas Correia <webdantas@gmail.com>
2023-05-02 11:23:03 -0300

    version-complete

--- resources/js/components/CaoUsuariosIndex.vue
+++ resources/js/components/CaoUsuariosIndex.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="container mx-auto px-4">
     <h1 class="text-xl font-bold mb-4">Usuários</h1>
+    <div class="flex items-center mb-4">
+    <label for="mes" class="mr-2">Mês:</label>
+      <select v-model="selectedMonth" name="mes" id="mes" class="mr-4 appearance-none text-black">
+        <option value="01">Janeiro</option>
+        <option value="02">Fevereiro</option>
+        <option value="03">Março</option>
+        <option value="04">Abril</option>
+        <option value="05">Maio</option>
+        <option value="06">Junho</option>
+        <option value="07">Julho</option>
+        <option value="08">Agosto</option>
+        <option value="09">Setembro</option>
+        <option value="10">Outubro</option>
+        <option value="11">Novembro</option>
+        <option value="12">Dezembro</option>
+      </select>
+      <label for="ano" class="mr-2">Ano:</label>
+      <select v-model="selectedYear" name="ano" id="ano" class="appearance-none text-black">
+        <option v-for="year in availableYears" :value="year">{{ year }}</option>
+      </select>
+    </div>
     <table class="w-full text-left">
       <thead>
         <tr>
@@ -29,9 +50,14 @@
         </tr>
       </tbody>
     </table>
-    <button @click="applyFilters" class="bg-blue-500 text-white px-4 py-2 mt-4">
+    <button @click="applyFilters" class="bg-blue-500 text-white px-4 py-2 mt-4 mr-2">
       Aplicar filtros
     </button>
+    <button @click="clearFilters" class="bg-red-500 text-white px-4 py-2 mt-4">
+      Limpar filtros
+    </button>
+
+
   </div>
 </template>
 
@@ -44,41 +70,84 @@ export default {
     },
   },
   data() {
-    console.log(this.caoUsuarios); // Adicione esta linha
     return {
       users: this.caoUsuarios,
       selectedUsers: [],
+      selectedMonth: null,
+      selectedYear: null,
+      filteredUsers: null,
+      availableYears: []
     };
   },
+
   methods: {
+    clearFilters() {
+      this.selectedMonth = '';
+      this.selectedYear = '';
+      this.selectedUsers = [];
+    },
+
     getReceitaLiquida(user) {
-    if (!user.caoOs || user.caoOs.length === 0) { // Verifica se o usuário tem ordens de serviço
-        console.log('caoOs is not available for user:', user);
-        return 0;
-    }
+      if (!user.caoOs || user.caoOs.length === 0) { // Verifica se o usuário tem ordens de serviço
+          console.log('caoOs is not available for user:', user);
+          return 0;
+      }
 
-    return user.caoOs.reduce((sum, os) => {
-        if (!os.caoFaturas || os.caoFaturas.length === 0) { // Verifica se a ordem de serviço tem faturas
-            console.log('caoFaturas is not available for os:', os);
-            return sum;
-        }
+      return user.caoOs.reduce((sum, os) => {
+          if (!os.caoFaturas || os.caoFaturas.length === 0) { // Verifica se a ordem de serviço tem faturas
+              console.log('caoFaturas is not available for os:', os);
+              return sum;
+          }
 
-        const osSum = os.caoFaturas.reduce((faturaSum, fatura) => {
-            console.log('fatura:', fatura);
-            return faturaSum + (fatura.valor_liquido || 0);
-        }, 0);
+          const osSum = os.caoFaturas.reduce((faturaSum, fatura) => {
+              console.log('fatura:', fatura);
+              return faturaSum + (fatura.valor_liquido || 0);
+          }, 0);
 
-        console.log('osSum:', osSum);
-        return sum + osSum;
-    }, 0);
-},
-
-
+          console.log('osSum:', osSum);
+          return sum + osSum;
+      }, 0);
+    },
 
     applyFilters() {
-        // Aqui você pode aplicar os filtros usando os usuários selecionados em "selectedUsers"
-        console.log(this.selectedUsers);
+      if (!this.selectedMonth || !this.selectedYear) {
+        this.filteredUsers = null;
+        this.users = this.caoUsuarios;
+        return;
+      }
+
+      const filteredUsers = this.caoUsuarios.filter(user => {
+        if (!user.caoOs) return false;
+        return user.caoOs.some(os => {
+          if (!os.caoFaturas) return false;
+          return os.caoFaturas.some(fatura => {
+            const faturaDate = new Date(fatura.data_emissao);
+            return (
+              faturaDate.getFullYear() === Number(this.selectedYear) &&
+              faturaDate.getMonth() === Number(this.selectedMonth) - 1
+            );
+          });
+        });
+      });
+
+      this.filteredUsers = filteredUsers;
+      this.users = filteredUsers;
+    },
+
+    clearFilters() {
+      this.selectedMonth = '';
+      this.selectedYear = '';
+      this.selectedUsers = [];
     },
   },
-};
+
+  mounted() {
+    const maxYear = new Date().getFullYear();
+    const minYear = 2009;
+
+    for (let i = maxYear; i >= minYear; i--) {
+      this.availableYears.push(i.toString());
+    }
+  },
+}
 </script>
